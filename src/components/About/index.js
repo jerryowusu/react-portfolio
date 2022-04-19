3bc3ceb46832df61162bebcc29a5dd8b538c80a6
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-04-19 06:51:06 -1200

    add about animations

---
+++ src/components/About/index.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import {
+    faAngular,
+    faCss3,
+    faGitAlt,
+    faHtml5,
+    faJsSquare,
+    faReact,
+  } from '@fortawesome/free-brands-svg-icons';
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+
+const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        // return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        // }, 2000)
+    }, [])
+
+    return (
+        <div className="container about-page">
+            <div className="text-zone">
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass} 
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                        index={15}
+                    />
+                </h1>
+                <p>I'm a very meticulous and ambituous full-stack developer looking for a role in an established Tech company which will help me grow and become the best whilst working with the latest technologies on various projects</p>
+                <p>I am proficiently confident, naturally curious, and strategically working towards improving and upgrading my skills one step at a time.</p>
+                <p>If I need to define myself in one sentence that will be a happy person, loving husband and caring father.</p>
+            </div>
+            <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+        </div>
+    )
+}
+
+export default About;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -30,7 +30,7 @@ const Home = () => {
                 <img src={Jerry} alt="jerry" />
                 <AnimatedLetters letterClass={letterClass} strArray={nameArray} index={15} />
                 <br />
-                <AnimatedLetters letterClass={letterClass} strArray={jobArray} index={19} />
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} index={20} />
                 </h1>
                 <h2>Full-stack Developer | JavaScript lover</h2>
                 <Link to="/contact" className='flat-button'>Contact Me</Link>
