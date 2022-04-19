7c719ddfbc27abdf022eaa30386cba74c25e59f6
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-04-19 08:39:40 -1200

    add map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,7 @@ import Loader from 'react-loaders';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useState } from 'react';
+// import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 
 const Contact = () => {
@@ -23,7 +24,6 @@ const Contact = () => {
                     <p>
                         I am interested in freelance and remote job opportunities especially ambitious and life-impacting projects. However, do not hesitate to contact me in the form below if you have any question
                     </p>
-                </div>
                 <div className="contact-form">
             <form >
               <ul>
@@ -59,6 +59,24 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+                </div>
+                <div className="info-map">
+                    Jerry Owusu,
+                    <br />
+                    Ghana,
+                    <br />
+                    Flower Pot, off the Spintex Road <br />
+                    Accra. <br />
+                    <span>jerryowusu6133@gmail.com</span>
+                </div>
+                {/* <div className="map-wrap">
+                    <MapContainer center={[44.96366, 19.61045]} zoom={15}>
+                        <TileLayer url='https//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+                        <Marker position={[44.96366, 19.61045]}>
+                            <Popup>Jerry stays around here, come over for a hearty chat :)</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div> */}
             </div>
             <Loader type='pacman' />
         </>
